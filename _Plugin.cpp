7457ecb428bda7218cb052b94baaee17fe6ab509
Eugene Kozlov <nekoeuge@gmail.com>
2025-10-29 13:12:18 +0100

    Implement SteamAudio based on https://github.com/rbfx/rbfx/pull/659

--- _Plugin.cpp
+++ _Plugin.cpp
@@ -1,7 +1,12 @@
 #include "_Plugin.h"
 
-#include "SampleComponent.h"
+#include "SteamAudio.h"
+#include "SteamSoundListener.h"
+#include "SteamSoundMesh.h"
+#include "SteamSoundSource.h"
 
+#include <Urho3D/Engine/Engine.h>
+#include <Urho3D/Engine/EngineDefs.h>
 #include <Urho3D/Plugins/PluginApplication.h>
 
 using namespace Urho3D;
@@ -11,11 +16,20 @@ namespace
 
 void RegisterPluginObjects(PluginApplication& plugin)
 {
-    plugin.RegisterObject<SampleComponent>();
+    plugin.RegisterObject<SteamSoundListener>();
+    plugin.RegisterObject<SteamSoundMesh>();
+    plugin.RegisterObject<SteamSoundSource>();
+
+    Context* context = plugin.GetContext();
+    auto steamAudio = context->RegisterSubsystem<SteamAudio>();
+    auto engine = context->GetSubsystem<Engine>();
+    steamAudio->SetMode(engine->GetParameter(EP_SOUND_MIX_RATE).GetInt(),
+        static_cast<SpeakerMode>(engine->GetParameter(EP_SOUND_MODE).GetInt()));
 }
 
 void UnregisterPluginObjects(PluginApplication& plugin)
 {
+    plugin.GetContext()->RemoveSubsystem<SteamAudio>();
 }
 
 } // namespace
